cd4921630e96f125a4f0ee6186eebc0cd726b944
Coder__Thanh <nguyevanthanh64992gmail.com>
2019-05-21 00:24:59 +0700

    init project

---
+++ src/layouts/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line no-unused-vars
+import NavigationRouter from '../components/navigationRouter.component';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <NavigationRouter />
+        );
+    }
+}
